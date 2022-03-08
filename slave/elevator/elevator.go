d6a6ae13570da50fde94d60d3c475c476efc9835
Thaleef <finkthale@gmail.com>
2022-03-08 16:46:55 +0100

    litt stuff

--- slave/elevator/elevator.go
+++ slave/elevator/elevator.go
@@ -1,47 +1,47 @@
-package elevator
+// package elevator
 
-import (
-	"Driver-go/elevio"
-)
+// import (
+// 	"Driver-go/elevio"
+// )
 
-type Elevator struct {
-	direction    elevio.MotorDirection
-	currentFloor int
-}
+// type Elevator struct {
+// 	direction    elevio.MotorDirection
+// 	currentFloor int
+// }
 
-func (e *Elevator) GetDirection() elevio.MotorDirection {
-	return e.direction
-}
-func (e *Elevator) GetCurrentFloor() int {
-	return e.currentFloor
-}
-func (e *Elevator) SetFloor(floor int) {
-	e.currentFloor = floor
-}
-func (e *Elevator) SetDirection(dir elevio.MotorDirection) {
-	e.direction = dir
-}
+// func (e *Elevator) GetDirection() elevio.MotorDirection {
+// 	return e.direction
+// }
+// func (e *Elevator) GetCurrentFloor() int {
+// 	return e.currentFloor
+// }
+// func (e *Elevator) SetFloor(floor int) {
+// 	e.currentFloor = floor
+// }
+// func (e *Elevator) SetDirection(dir elevio.MotorDirection) {
+// 	e.direction = dir
+// }
 
-func (e *Elevator) DriveTo(order elevio.ButtonEvent) {
+// func (e *Elevator) DriveTo(order elevio.ButtonEvent) {
 
-	var elevDir elevio.MotorDirection
-	var motorDir elevio.MotorDirection
+// 	var elevDir elevio.MotorDirection
+// 	var motorDir elevio.MotorDirection
 
-	if e.GetCurrentFloor() < order.Floor {
-		motorDir = elevio.MD_Up
-		elevDir = motorDir
-	} else if e.GetCurrentFloor() > order.Floor {
-		motorDir = elevio.MD_Down
-		elevDir = motorDir
-	} else {
-		motorDir = elevio.MD_Stop
-		if order.Button == elevio.BT_HallUp {
-			elevDir = elevio.MD_Up
-		} else if order.Button == elevio.BT_HallDown {
-			elevDir = elevio.MD_Down
-		}
-	}
+// 	if e.GetCurrentFloor() < order.Floor {
+// 		motorDir = elevio.MD_Up
+// 		elevDir = motorDir
+// 	} else if e.GetCurrentFloor() > order.Floor {
+// 		motorDir = elevio.MD_Down
+// 		elevDir = motorDir
+// 	} else {
+// 		motorDir = elevio.MD_Stop
+// 		if order.Button == elevio.BT_HallUp {
+// 			elevDir = elevio.MD_Up
+// 		} else if order.Button == elevio.BT_HallDown {
+// 			elevDir = elevio.MD_Down
+// 		}
+// 	}
 
-	e.SetDirection(elevDir)
-	elevio.SetMotorDirection(motorDir)
-}
+// 	e.SetDirection(elevDir)
+// 	elevio.SetMotorDirection(motorDir)
+// }
