cef838200f1262fe51a151facb4a474bae5c27da
Thaleef <finkthale@gmail.com>
2022-03-09 16:15:41 +0100

    Fiksa veldig mykje..

---
+++ Driver-go/main.go
@@ -0,0 +1,21 @@
+package main
+
+import(
+	"Driver-go/elevio"
+	"Driver-go/elevator"
+	//"Driver-go/slaveFSM"
+)
+// import (
+// 	"Driver-go/elevator"
+// 	"Driver-go/elevio"
+// 	"Driver-go/slaveFSM"
+// )
+
+func main() {
+
+}
+// func main(){
+// 	while(1)(
+// 		slaveFSM.slaveFSM()
+// 	)
+// }
