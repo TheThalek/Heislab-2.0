fa0f835d0350c5ddb8687b50d78f065460b44d67
Thaleef <finkthale@gmail.com>
2022-03-09 10:08:38 +0100

    Innhold til init()

--- slave/singleFSM/slaveFSM.go
+++ slave/singleFSM/slaveFSM.go
@@ -1,5 +1,28 @@
-//Make state machine
+package slaveFSM
 
+import (
+	"slave/elevator"
+	"slave/elevator.io"
+)
+
+numFloors := 4 //To do; Kanskje gjør denne "global", då den blir brukt av alle?
+
+
+func init() {
+	var myElevator elevator.Elevator
+
+	var orderPanel [orders.ConstNumFloors][3]int
+
+	elevio.Init("localhost:15657", numFloors)
+
+	elevFSM.RunElevFSM(numFloors, myElevator, orderPanel)
+	
+	elevio.SetMotorDirection(elevio.MD_Down)
+
+	var doorOpen bool = false
+	var moving bool = true
+	var obs bool = false
+}
 
 //Tar inn ein Elevator-struct, som 
 func slaveFSM() {
@@ -7,8 +30,24 @@ func slaveFSM() {
 	//Sende ut: på en kanal(?) posisjon, 
 	//om den har tatt en ordre/er ferdig, 
 	//direction, obstruksjon, 
+
+	init()
 	
-	(elevator *Elevator)
+	var priorityOrder elevio.ButtonEvent
+	priorityOrder.Floor = -1
+
+	for f := 0; f < numFloors; f++ {
+		for b := 0; b < 3; b++ {
+			elevio.SetButtonLamp(elevio.ButtonType(b), f, false)
+		}
+	}
 }
+
+
+func main() {
+
+}
+
+
 
 
