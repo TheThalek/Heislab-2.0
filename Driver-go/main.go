6c18b4c29d84fd280a7767fd4aa127150e57089d
thofjord <olerum00@gmail.com>
2022-03-11 13:43:41 +0100

    fiksa main.go

--- Driver-go/main.go
+++ Driver-go/main.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package main
 
 import (
@@ -10,12 +9,3 @@ func main() {
 	singleFSM.thaleSinMain()
 	//networking.pederSinMain()
 }
-=======
-package main
-
-func main() {
-	//MS_FSM.maikenSinMain()
-	//singleFSM.thaleSinMain()
-	//network.pederSinMain()
-}
->>>>>>> d905676f0c4ae552b2f01a6489102e4f8adcef51
