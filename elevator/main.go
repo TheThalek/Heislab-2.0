674c9f5b73cb617d83a447e3a8328ec0de4a79dd
pjfoss <pederjoseffoss@gmail.com>
2022-03-23 11:43:52 +0100

    Update main.go

--- elevator/main.go
+++ elevator/main.go
@@ -4,10 +4,12 @@ package main
 //Run alle filer: go run *.go
 //Evt build and execute: go build && ./Elevator
 
+//om du vil kjøre en og en, eller kun noen, go run .\main.go .\\Vår\Sanntidsprogrammerelevator.go .\orderLogic.go .\networking.go
 func main() {
 	//MS_FSM.maikenSinMain()
 	//singleFSM.ThaleSinMain()
 	go PederSinMain()
 	//MS_FSM.RunSystemFSM()
-	for{}
+	for {
+	}
 }
