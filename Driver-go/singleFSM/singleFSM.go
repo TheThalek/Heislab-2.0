7bec671d80b3979134528fdb69a4301d149bf9a9
Thaleef <finkthale@gmail.com>
2022-03-23 15:21:42 +0100

    Masse i singleElevator

--- Driver-go/singleFSM/singleFSM.go
+++ Driver-go/singleFSM/singleFSM.go
@@ -3,7 +3,7 @@ package singleFSM
 import (
 	"Driver-go/elevator"
 	"Driver-go/elevio"
-	"Driver-go/orders"
+	//"Driver-go/orders"
 )
 
 
@@ -18,42 +18,31 @@ const (
 
 func ThaleSinMain() {
 	slaveFSMinit()
-
-	var masterOrderPanel [orders.ConstNumFloors][orders.ConstNumElevators+2]int
+	fmt.Println("Test")
+	var masterOrderPanel [elevator.NUMBER_OF_FLOORS][NUMBER_OF_COLUMNS]int
 
 	var localElevator elevator.Elevator
 
 	go slaveFSM(&localElevator, masterOrderPanel)
-
 }
 
 
-func slaveFSMinit() {
-
-	//Make connection with local elevator, to make it run
-	elevio.Init("localhost:15657", orders.ConstNumFloors)
+func SlaveFSMinit() {
+	elevio.Init("localhost:15657", elevator.NUMBER_OF_FLOORS)
 
 	elevio.SetMotorDirection(elevio.MD_Down)
 
-	// var doorOpen bool = false
-	// var moving bool = true
-	// var obs bool = false
-	// Sett start state lik noko?? Evt. ha ein default i state-machinen i SlaveFSM
-
-	for f := 0; f < orderrs.ConstNumFloors; f++ {
+	for f := 0; f < elevator.NUMBER_OF_FLOORS; f++ {
 		for b := 0; b < 3; b++ {
 			elevio.SetButtonLamp(elevio.ButtonType(b), f, false)
 		}
 	}
-
-	//Og kjøre nedover til den når den nederste etasjen sin!
-	
 }
 
-func setLights(masterOrderPanel [orders.ConstNumFloors][orders.ConstNumElevators+2]int) {
-	for f := 0; f < numFloors; f ++{
+func setLights(masterOrderPanel [elevator.NUMBER_OF_FLOORS][elevator.NUMBER_OF_COLUMNS]int) {
+	for f := 0; f < elevator.NUMBER_OF_FLOORS; f ++ {
 		for b := 0; b < 3; b++ {
-			if ((b = 0)||(b = 1)) { //If up or down pushed
+			if b = 0 || b = 1 { //If up or down pushed
 				elevio.SetButtonLamp(elevio.ButtonType(b), f, (masterOrderPanel[f][b]!=OT_NoOrder)) //Will set the lamp on/off if 0/1or2
 			} else if (b = 2) { //If cab 
 				elevio.SetButtonLamp(elevio.ButtonType(b), f, (masterOrderPanel[f][getElevatorIndex() + 2])!=OT_NoOrder)) //GetElevatorIndex gives the nr. of column
@@ -62,48 +51,108 @@ func setLights(masterOrderPanel [orders.ConstNumFloors][orders.ConstNumElevators
 	}
 }
 
-func slaveFSM(localElevator *elevator.Elevator, masterOrderPanel [orders.ConstNumFloors][orders.ConstNumElevators+2]int) {
-	//Skru lysene på/av ut ifrå masterOrderPanel som kontinuerlig blir tatt inn
-	setLights(masterOrderPanel)
+func pollPriFloor() {
+	for {
+		priChan <- localElevator.GetPriOrder()
+	}
+}
+
+func SlaveFSM(localElevator *elevator.Elevator, masterOrderPanel [elevator.NUMBER_OF_FLOORS][elevator.NUMBER_OF_COLUMNS]int, takenOrders <-chan []elevio.ButtonEvent, newOrders <-chan []elevio.ButtonEvent) {
 	
-	drv_buttons := make(chan elevio.ButtonsEvent)
+	var state SlaveState = Idle
+	var currentDirection = MD_Down
+	localElevator.setDirection(currentDirection)
+
+	//Og kjøre nedover til den når den nederste etasjen sin!
+
+	drv_buttons := make(chan elevio.ButtonEvent)
 	drv_floors := make(chan int)
 	drv_obstr := make(chan bool)
+	priOrderChan := make(chan elevio.ButtonEvent)
 
 	go elevio.PollButtons(drv_buttons)
 	go elevio.PollFloorSensor(drv_floors)
 	go elevio.PollObstructionSwitch(drv_obstr)
+	go pollPriFloor(priOrderChan) 
+	go setLights(masterOrderPanel)
 
-	for {
+	for {		
+		if (SlaveState = Move) {
+			//Køyr til etasjen du skal til OG du må endre direction du går i (i localElevator), dersom du endrer denne!
+			driveTo(&localElevator)
+		} 
+
 		select {
 		case obstr := <-drv_obstr:
-			//Lagre obstruction i elevator_structen vår
-			//localElevator.SetObs(elevio.getObstruction()) KANSKJE ENDRE SLIK AT DEN ER PEKER(?)
+			switch {
+			case SlaveState = Move:
+				elevio.SetMotorDirection(elevio.MD_Stop)
+				localElevator.setobs(true)
+				SlaveState = Obstruction
+			case SlaveState = Idle: 
+				localElevator.setobs(true)
+				SlaveState = Obstruction
+			case SlaveState = Obstruction:
+				if (localElevator.GetPriOrder() = OT_NoOrder) {
+					localElevator.setobs(false)
+					SlaveState = Idle
+				} else {
+					localElevator.setobs(false)
+					SlaveState = Move
+				}
+			}
 
-		case newfloor := <-drv_floors:
-			fmt.Printf("%+v\n", a)
-			//Oppdater etasjelys og elevator-objektet, slik at masterFSM veit kor du er
+		case newFloor := <-drv_floors:
+			localElevator.SetFloor(newFloor)
+			SetFloorIndicator(newFloor)
+
+			if newFloor = localElevator.GetPriOrder().Floor {
+				elevio.SetMotorDirection(elevio.MD_Stop)
+				SetDoorOpenLamp(true)
+				time.Sleep(3 * time.Second)
+				SetDoorOpenLamp(false)
+				cabOrder := elevio.ButtonEvent{
+					Floor: newFloor, 
+					Button: elevio.ButtonType(2),
+				}
+				if (localElevator.GetDirection() = MD_Down) {
+					dirOrder := elevio.ButtonEvent{
+						Floor: newFloor,
+						Button:elevio.ButtonType(1),
+					}
+	
+				} else if (localElevator.GetDirection() = MD_Up) {
+					dirOrder := elevio.ButtonEvent{
+						Floor: newFloor,
+						Button:elevio.ButtonType(0),
+					}
+				}
+				takenOrders <- [cabOrder, dirOrder]
+				SlaveState = Idle
+			}
 
 		case newButtons := <-drv_buttons:
-			//Send informasjon om at knappen har blitt tatt, til masterFSM, 
-		
-		case newPriority := <-priOrderChan:
-			driveTo(newPriority)
+			newOrders <- newButtons
+
+		case priority := <-priOrderChan:
+			if (priority.Floor = -1) { 
+				SlaveState = Idle
+			} else {
+				SlaveState = Move
+			}
 		}
-
-	}
-
-	switch SlaveState {
-	case Idle:
-		if (priOrder != OT_NoOrder) {
-			slaveState := Move
-		}
-	case Move: 
-
-	case Obstruction:
-		
-	default: 
-		slaveState := Idle
 	}
 }
 
+func driveTo(localElevator *elevator.Elevator) {
+	var int lastFloor = localElevator.getFloor()
+	var int newFloor = localElevator.GetPriOrder().Floor
+
+	if newFloor < lastFloor {
+		elevio.SetMotorDirection(elevio.MD_Down)
+		localElevator.setDirection(MD_Down)
+	} else if newFloor < lastFloor {
+		elevio.SetMotorDirection(elevio.MD_Up)
+		localElevator.setDirection(MD_Up)
+	}
+}
