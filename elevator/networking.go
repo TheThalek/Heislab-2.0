7fefd2a256a7d94fdf281fff3d084dde3a4519e0
pjfoss <pederjoseffoss@gmail.com>
2022-03-26 23:50:49 +0100

    working master-slave switch

--- elevator/networking.go
+++ elevator/networking.go
@@ -139,9 +139,7 @@ func ReportMasterTimeOut(masterTimeOutChan chan<- string, reset <-chan string) {
 
 func NetworkSortPeers(peers []string) []string {
 
-	var sortedPeers []string
-
-	sortedPeers = append(sortedPeers, peers[0])
+	var sortedPeers []string = []string{peers[0]}
 
 	if len(peers) == 1 {
 		return sortedPeers
@@ -153,12 +151,26 @@ func NetworkSortPeers(peers []string) []string {
 		for {
 			cmp, _ := strconv.Atoi(strings.Split(peers[j], "-")[2])
 			if val < cmp || j+1 == len(sortedPeers) {
+				j = j + 1
 				break
 			}
+			j = j + 1
 		}
-		sortedPeers = append(append(sortedPeers[0:j], peers[j]), sortedPeers[j+1:]...)
+		var temp []string
+		if j == 0 {
+			temp = []string{peers[i]}
+			temp = append(temp, sortedPeers...)
+		} else if j == len(sortedPeers)-1 {
+			temp = sortedPeers
+			temp = append(temp, peers[i])
+		} else {
+			temp = sortedPeers[:j]
+			temp = append(temp, "0")
+			temp = append(temp, sortedPeers[j:]...)
+			temp[j] = peers[i]
+		}
+		sortedPeers = temp
 	}
-
 	return sortedPeers
 }
 
@@ -171,7 +183,6 @@ var nOrders []elevio.ButtonEvent = []elevio.ButtonEvent{
 var cOrders []elevio.ButtonEvent = nOrders
 
 func NetworkConnect(id string) string {
-
 	if id == "" {
 		localIP, err := localip.LocalIP()
 		if err != nil {
@@ -180,7 +191,6 @@ func NetworkConnect(id string) string {
 		}
 		id = fmt.Sprintf("peer-%s-%d", localIP, os.Getpid())
 	}
-
 	return id
 }
 
@@ -208,25 +218,20 @@ func PederSinMain() {
 	//  `go run main.go -id=our_id`
 	var id string
 
+	var networkPeers []string
+	networkPeers = append(networkPeers, id)
+
 	flag.StringVar(&id, "id", "", "id of this peer")
 	flag.Parse()
 	id = NetworkConnect(id)
 
-	// We make a channel for receiving updates on the id's of the peers that are
-	//  alive on the network
 	peerUpdateCh := make(chan peers.PeerUpdate)
-	// We can disable/enable the transmitter after it has been started.
-	// This could be used to signal that we are somehow "unavailable".
 	peerTxEnable := make(chan bool)
 	go peers.Transmitter(15647, id, peerTxEnable)
 	go peers.Receiver(15647, peerUpdateCh)
 
-	// We make channels for sending and receiving our custom data types
 	msgTx := make(chan NetworkMessage)
 	msgRx := make(chan NetworkMessage)
-	// ... and start the transmitter/receiver pair on some port
-	// These functions can take any number of channels! It is also possible to
-	//  start multiple transmitters/receivers on the same port.
 	go bcast.Transmitter(16569, msgTx)
 	go bcast.Receiver(16569, msgRx)
 
@@ -248,37 +253,35 @@ func PederSinMain() {
 			}
 			time.Sleep(PERIOD)
 		}
-
 	}()
 
 	fmt.Println("Started")
 	for {
-		id = NetworkConnect("")
+		idSplit := strings.Split(id, "-")
+		ip := idSplit[1]
+		if ip == "DISCONNECTED" {
+
+		}
 		select {
 		case p := <-peerUpdateCh:
-			fmt.Printf("Peer update:\n")
-			fmt.Printf("  Peers:    %q\n", NetworkSortPeers(p.Peers))
-
-			fmt.Printf("  New:      %q\n", p.New)
-			fmt.Printf("  Lost:     %q\n", p.Lost)
+			networkPeers = NetworkSortPeers(p.Peers)
+			fmt.Println("Peer update: ")
+			fmt.Println("  Peers:    %q\n", networkPeers)
+			fmt.Println("  New:      %q\n", p.New)
+			fmt.Println("  Lost:     %q\n", p.Lost)
 
 		case a := <-msgRx:
 			b := StringToNetworkMsg(a.MessageString)
-			if b.Origin == MO_Master {
+			if b.Origin == MO_Master && len(networkPeers) > 1 {
 				resetMasterTimeOut <- "reset"
-			}
-			if b.ID != id {
-				if b.Origin == MO_Master {
-					info := ExtractMasterInformation(b, int(NUMBER_OF_FLOORS), int(NUMBER_OF_BUTTONS), int(NUMBER_OF_ELEVATORS))
-					fmt.Printf(id, " Received: %#v\n", info.OrderPanel)
-				} else {
-					info := ExtractSlaveInformation(b)
-					fmt.Printf(id, " Received: %#v\n", info.CompletedOrders)
-				}
+			} else if len(networkPeers) == 1 {
+				state = 0
 			}
 		case <-mTimeout:
-			fmt.Println("Master Timeout")
-			state = 1
+			fmt.Println(id, ">> Master Timeout")
+			if id == networkPeers[0] {
+				state = 1
+			}
 		}
 	}
 }
