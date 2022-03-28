b04033efaf3e2d98776b544b69c8be7eedb53731
TheThalek <80036163+TheThalek@users.noreply.github.com>
2022-03-28 18:40:06 +0200

    Kode og forsøk på testing og testfunksjoner

--- elevator/localElevator.go
+++ elevator/localElevator.go
@@ -36,6 +36,7 @@ func ThaleSinMain() {
 func LocalInit() {
 	elevio.Init("localhost:15657", NUMBER_OF_FLOORS)
 	elevio.SetMotorDirection(elevio.MD_Down)
+	elevio.SetDoorOpenLamp(false)
 
 	for f := 0; f < NUMBER_OF_FLOORS; f++ {
 		for b := 0; b < 3; b++ {
@@ -66,14 +67,29 @@ func pollPriFloor(priOrder chan elevio.ButtonEvent, myElevator *Elevator) {
 }
 
 func test(myElevator *Elevator) {
-	fmt.Println("before 5 sec")
+	fmt.Println("before 5 sec 1 ")
 	time.Sleep(5 * time.Second)
-	fmt.Println("after 5 sec")
-	testPri := elevio.ButtonEvent{
+	fmt.Println("after 5 sec 1")
+	testPri1 := elevio.ButtonEvent{
 		Floor:  3,
 		Button: elevio.ButtonType(0),
 	}
-	myElevator.SetPriOrder(testPri)
+	myElevator.SetPriOrder(testPri1)
+
+	fmt.Println("before 5 sec 2")
+	time.Sleep(15 * time.Second)
+	fmt.Println("after 5 sec 2")
+	testPri2 := elevio.ButtonEvent{
+		Floor:  1,
+		Button: elevio.ButtonType(0),
+	}
+	myElevator.SetPriOrder(testPri2)
+}
+
+func FloorIntTest(elevIndex chan int) {
+	for {
+		elevIndex <- 1
+	}
 }
 
 func LocalControl(myElevator *Elevator, masterOrderPanel [NUMBER_OF_FLOORS][NUMBER_OF_COLUMNS]int, takenOrders chan []elevio.ButtonEvent, newOrders chan elevio.ButtonEvent, elevIndex chan int) {
@@ -87,11 +103,15 @@ func LocalControl(myElevator *Elevator, masterOrderPanel [NUMBER_OF_FLOORS][NUMB
 	drv_obstr := make(chan bool)
 	priOrderChan := make(chan elevio.ButtonEvent)
 
+	//TESTER
+	go test(myElevator)
+	go FloorIntTest(elevIndex)
+
 	go elevio.PollButtons(drv_buttons)
 	go elevio.PollFloorSensor(drv_floors)
 	go elevio.PollObstructionSwitch(drv_obstr)
 	go pollPriFloor(priOrderChan, myElevator)
-	go test(myElevator)
+
 	//go setLights(masterOrderPanel, <-elevIndex) //TO DO; ha med en "polingsfunksjon" i main
 
 	for {
@@ -124,6 +144,7 @@ func LocalControl(myElevator *Elevator, masterOrderPanel [NUMBER_OF_FLOORS][NUMB
 			}
 
 		case newFloor := <-drv_floors:
+			fmt.Println("drv_floors")
 			myElevator.SetFloor(newFloor)
 			elevio.SetFloorIndicator(newFloor)
 			switch {
@@ -134,6 +155,7 @@ func LocalControl(myElevator *Elevator, masterOrderPanel [NUMBER_OF_FLOORS][NUMB
 			case state == Move:
 				fmt.Println("drv_floors Move")
 				if newFloor == myElevator.GetPriOrder().Floor {
+					fmt.Println("I have an order here!")
 					elevio.SetMotorDirection(elevio.MD_Stop)
 					elevio.SetDoorOpenLamp(true)
 					time.Sleep(3 * time.Second)
@@ -160,44 +182,12 @@ func LocalControl(myElevator *Elevator, masterOrderPanel [NUMBER_OF_FLOORS][NUMB
 						}
 						completedOrders = append(completedOrders, dirOrder)
 					}
-
+					fmt.Println("Completed order")
 					takenOrders <- completedOrders
+					fmt.Println("Sent to takenOrders")
 					state = Idle
+					fmt.Println("After setting equal to Idle")
 				}
-			}
-			myElevator.SetFloor(newFloor)
-			elevio.SetFloorIndicator(newFloor)
-
-			if newFloor == myElevator.GetPriOrder().Floor {
-				elevio.SetMotorDirection(elevio.MD_Stop)
-				elevio.SetDoorOpenLamp(true)
-				time.Sleep(3 * time.Second)
-				elevio.SetDoorOpenLamp(false)
-
-				var completedOrders []elevio.ButtonEvent
-				if masterOrderPanel[newFloor][<-elevIndex+2] != OT_NoOrder {
-					cabOrder := elevio.ButtonEvent{
-						Floor:  newFloor,
-						Button: elevio.ButtonType(2),
-					}
-					completedOrders = append(completedOrders, cabOrder)
-				}
-				if (masterOrderPanel[newFloor][0] != OT_NoOrder) && (myElevator.GetDirection() == elevio.MD_Up) {
-					dirOrder := elevio.ButtonEvent{
-						Floor:  newFloor,
-						Button: elevio.ButtonType(0),
-					}
-					completedOrders = append(completedOrders, dirOrder)
-				} else if (masterOrderPanel[newFloor][1] != OT_NoOrder) && (myElevator.GetDirection() == elevio.MD_Down) {
-					dirOrder := elevio.ButtonEvent{
-						Floor:  newFloor,
-						Button: elevio.ButtonType(1),
-					}
-					completedOrders = append(completedOrders, dirOrder)
-				}
-
-				takenOrders <- completedOrders
-				state = Idle
 			}
 
 		case newButtons := <-drv_buttons:
@@ -235,6 +225,8 @@ func driveTo(myElevator *Elevator, floorChan chan int) {
 		elevio.SetMotorDirection(elevio.MD_Up)
 		myElevator.SetDirection(elevio.MD_Up)
 	} else if newFloor == lastFloor { //Hvis den er i rett etasje!
+		fmt.Println("b4 Sending to drv_floors")
 		floorChan <- newFloor
+		fmt.Println("after Sending to drv_floors")
 	}
 }
